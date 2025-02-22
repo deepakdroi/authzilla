7d9e1bca9832c2c8389146a2d8e895cae5c7fcdd
deepakdroi <deepak.mahto706@gmail.com>
2025-02-23 00:16:05 +0530

    feat: Update login form to comply with new schema and AuthJS
    - Updated login form fields to align with the new user schema
    - Integrated Zod validation schema for form validation
    - Implemented AuthJS for user authentication
    - Enhanced error handling and validation feedback
    
    This commit ensures the login process is in line with the updated schema and utilizes AuthJS for secure authentication.

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,163 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Padlock from "@/assets/images/rb_548.png";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { loginSchema, LoginSchema } from "@/lib/schemas/loginSchema";
+import { EyeSlashFilledIcon } from "@/components/passwordVisibility/EyeSlashedFilledIcon";
+import { EyeFilledIcon } from "@/components/passwordVisibility/EyeFilledIcon";
+import { SubmitButton } from "@/components/SubmitButton";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { signInUser } from "@/app/actions/authActions";
+import { toast } from "react-toastify";
+
+export default function Login() {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitting },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = async (data: LoginSchema) => {
+    const result = await signInUser(data);
+    if (result.status === "success") {
+      router.push("/");
+    } else {
+      console.log(result.error);
+      toast.error(result.error as string);
+    }
+  };
+
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Link
+          href="/"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <Image className="w-8 h-8 mr-2" src={Padlock} alt="logo" />
+          Authzilla
+        </Link>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <form
+              className="space-y-4 md:space-y-6"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  {...register("email")}
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required
+                />
+                {errors.email && (
+                  <p className="text-xs text-red-500 dark:text-red-400">
+                    {errors.email?.message}
+                  </p>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <div className="relative">
+                  <input
+                    type={isVisible ? "text" : "password"}
+                    {...register("password")}
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+                  <button
+                    className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
+                    type="button"
+                    onClick={toggleVisibility}
+                    aria-label="toggle password visibility"
+                  >
+                    {isVisible ? (
+                      <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                    ) : (
+                      <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                    )}
+                  </button>
+                </div>
+                {errors.password && (
+                  <p className="text-xs text-red-500 dark:text-red-400">
+                    {errors.password?.message}
+                  </p>
+                )}
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                      required
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label
+                      htmlFor="remember"
+                      className="text-gray-500 dark:text-gray-300"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+                <Link
+                  href="#"
+                  className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+                >
+                  Forgot password?
+                </Link>
+              </div>
+              <SubmitButton
+                isValid={isValid}
+                isSubmitting={isSubmitting}
+                actionType="login"
+              />
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don&aps;t have an account yet?{" "}
+                <Link
+                  href="/signup"
+                  className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                >
+                  Sign up
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
