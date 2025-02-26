e91aa0c2a1a5f68a5b17f1dd94839728701f9107
deepakdroi <deepak.mahto706@gmail.com>
2025-02-27 00:32:22 +0530

    Fix: Address inconsistent middleware functioning, excessive redirection, and correct naming issues
    - Resolved issue with middleware causing inconsistent behavior
    - Reduced the number of unnecessary redirections
    - Corrected naming conventions for improved clarity and maintainability

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,7 +13,7 @@ export default auth((req) => {
 
   if (isAuthRoute) {
     if (isLoggedIn) {
-      return NextResponse.redirect(new URL("/members", nextUrl));
+      return NextResponse.redirect(new URL("/securedpage", nextUrl));
     }
     return NextResponse.next();
   }
